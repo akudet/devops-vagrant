19daa7d9c15028e7353f4a946626feb2e68e36e0
Jintong Wu <akudet@qq.com>
2019-05-04 12:09:21 +0800

    cluster : add kubeadm config

--- config/init.sh
+++ config/init.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env bash
 
-sudo cp /home/vagrant/config/sources.list /etc/apt/sources.list
-
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/cache/apt/archives/lock
 sudo rm /var/lib/dpkg/lock
 
+sudo cp /home/vagrant/config/apt/sources.list /etc/apt/sources.list
+
 sudo apt update
 
-sudo apt-get install -y \
+sudo apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+sudo cp /home/vagrant/config/apt/sources.list.d/docker.list /etc/apt/sources.list.d/
 
-sudo apt-get update
-sudo apt-get install -y docker-ce
+curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo cp /home/vagrant/config/apt/sources.list.d/kubernetes.list /etc/apt/sources.list.d/
 
-dockerd --registry-mirror=https://registry.docker-cn.com
+sudo apt update
+sudo apt install -y docker-ce
 
 sudo groupadd docker
 sudo usermod -aG docker vagrant
+
+sudo apt install -y kubeadm
+
