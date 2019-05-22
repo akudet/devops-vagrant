31aa4b5d6357bdda47914e7a67ba9fac37c8b14e
Jintong Wu <akudet@qq.com>
2019-05-22 17:13:50 +0800

    cluster : add docker credential for jenkins

--- config/jenkins.sh
+++ config/jenkins.sh
@@ -4,12 +4,15 @@ docker service create --name jenkins \
   --publish 8080:8080 --publish 50000:50000 \
   --mount src=jenkins_home,dst=/var/jenkins_home \
   jenkins/jenkins:lts
-  
+
 docker run \
-  --rm -d \
+  --name jenkins \
+  -d \
   -u root \
   -p 5080:8080 \
-  -v ~/jenkins_blueocean:/var/jenkins_home \
+  --restart=always \
+  -v ~/.ssh:/root/.ssh \
+  -v ~/.docker:/root/.docker \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v ~/jenkins_blueocean_home:/home \
+  -v ~/jenkins_blueocean:/var/jenkins_home \
   jenkinsci/blueocean
